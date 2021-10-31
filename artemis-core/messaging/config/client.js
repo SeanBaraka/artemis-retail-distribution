6ca3711d684129474635a53acf6635c9dcc6bcc4
SeanBaraka <seanbaraka@gmail.com>
2021-10-31 14:07:56 +0300

    added local brokers

--- artemis-core/messaging/config/client.js
+++ artemis-core/messaging/config/client.js
@@ -2,8 +2,8 @@ import { Kafka } from "kafkajs";
 import { createClient } from "redis";
 
 export const kafka = new Kafka({
-    brokers: ['localhost:9092'],
-    clientId: 'artemis-core',
+    brokers: ['localhost:9092', 'localhost:9093', 'localhost:9094'],
+    clientId: 'artemis-core'
 })
 
 export const redis = createClient(6379)
